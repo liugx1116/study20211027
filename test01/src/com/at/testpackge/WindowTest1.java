a191e2bdb52251cf5e9a53b6efe31df6b2f0ac32
liugx <sunshine_lgx@163.com>
2021-10-27 12:44:16 +0800

    多线程实现3个窗口出售100张票

---
+++ test01/src/com/at/testpackge/WindowTest1.java
@@ -0,0 +1,33 @@
+package com.at.testpackge;
+
+public class WindowTest1 {
+    public static void main(String[] args) {
+        Window1 window1 = new Window1();
+        Thread t1 = new Thread(window1);
+        Thread t2 = new Thread(window1);
+        Thread t3 = new Thread(window1);
+        t1.setName("窗口1");
+        t2.setName("窗口2");
+        t3.setName("窗口3");
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+}
+
+class Window1 implements Runnable{
+    private int ticket = 100;
+    @Override
+    public void run() {
+        while(true){
+            if (ticket >0) {
+                System.out.println(Thread.currentThread().getName()+ ":卖票，票号为：" + ticket);
+                ticket--;
+            }else {
+                break;
+            }
+
+        }
+    }
+}
